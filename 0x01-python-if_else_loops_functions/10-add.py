b92bcf5b009c5810b7a0560071283f1a47ec90eb
razorpro9991 <145159604+razorpro9991@users.noreply.github.com>
2024-01-04 04:20:54 +0000

    rest of the tasks

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 10-add.py
+# Ebenezer Kissiedu <Kissieduebenezer1@gmail.com>
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Ebenezer Kissiedu <Kissieduebenezer1@gmail.com>
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
