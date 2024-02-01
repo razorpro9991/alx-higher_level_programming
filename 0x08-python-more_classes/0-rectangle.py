787ecf177fa02f4c739ebe2caa9ba217352b36b5
razorpro9991 <145159604+razorpro9991@users.noreply.github.com>
2024-02-01 05:37:24 +0000

    tasks

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Ebenezer Kissiedy<kissieduebenezer1@gmail.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
