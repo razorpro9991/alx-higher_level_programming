2f509d9f2c4c256c8bd42a910f20004932317e85
razorpro9991 <145159604+razorpro9991@users.noreply.github.com>
2024-02-05 12:39:18 +0000

    tasks

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Ebenezer Kissiedu<kissieduebenezer1@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
